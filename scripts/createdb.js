a6b09984c94fb9997da61b0330982a8a3643fa09
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2016-08-28 15:44:07 -0500

    Close connection after generating database

--- scripts/createdb.js
+++ scripts/createdb.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 var fs = require('fs');
-var sqlite3 = require('sqlite3')
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database(process.env.DB_NAME);
 
 fs.readFile('./db/createdb.sql', 'utf8', function(err, data) {
 	if (err) return console.log(err);
 
-	db.exec(data);
+	db.exec(data, function() {
+		db.close();
+	});
 });
